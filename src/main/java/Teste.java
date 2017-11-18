cc98c04d278a3c324109170bc56f54954e4f41d3
Ednelson Rodrigues <edrodrigues.inf@gmail.com>
2017-11-18 12:11:19 -0200

    Sistema para gerenciamento de processos jurídicos

---
+++ src/main/java/Teste.java
@@ -0,0 +1,21 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("advsoftPU");
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction t = em.getTransaction();
+		
+		t.begin();
+		
+		
+		
+		t.commit();
+	}
+
+}
